79f2c2f19b1f310ce8df6fbd78a03e048d2d7fc8
Adel djahnit <adeldjahnita@gmail.com>
2025-08-22 23:24:14 +0200

    feat/end

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: true, 
+    origin: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: false,
